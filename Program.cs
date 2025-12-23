d708bb50fa9cbe6d029e4e4b62f9daf208d2ab33
tranvlnh <92441399+tranvlnh26@users.noreply.github.com>
2025-12-23 18:13:52 +0700

    Initial project setup for MeowBot
    Add base .NET 9.0 project for MeowBot with Docker support, solution and project files, basic Discord bot implementation using NetCord, configuration files, and Docker Compose setup.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Hosting;
+
+using NetCord.Hosting.Gateway;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+    .AddDiscordGateway();
+var host = builder.Build();
+
+await host.RunAsync();
