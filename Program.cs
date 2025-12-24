c9255751b25c1851fc6ad5578b0e8a1df062020b
tranvlnh <92441399+tranvlnh26@users.noreply.github.com>
2025-12-24 18:38:30 +0700

    feat: Add Npgsql and DI for Guild services and repositories
    Added Npgsql and Npgsql.DependencyInjection packages to support PostgreSQL integration. Updated Program.cs to register Npgsql data source, GuildRepository, and GuildService with dependency injection. Also cleaned up unused folder includes in the project file.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿using MeowBot.Modules;
+﻿using MeowBot.Abstractions.Repositories;
+using MeowBot.Abstractions.Services;
+using MeowBot.Infrastructure.Repositories;
+using MeowBot.Modules;
+using MeowBot.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NetCord.Hosting.Gateway;
 using NetCord.Hosting.Services.ApplicationCommands;
 
+
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services
-    .AddDiscordGateway()
-    .AddApplicationCommands();
+var services = builder.Services;
+services.AddDiscordGateway()
+        .AddApplicationCommands()
+        .AddNpgsqlDataSource(builder.Configuration["PostgresConnection"]!)
+        .AddSingleton<IGuildRepository, GuildRepository>()
+        .AddSingleton<IGuildService, GuildService>();
+
 
 var host = builder.Build();
 
